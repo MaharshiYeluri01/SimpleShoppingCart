fc5adff7362bb8d8503d20f1e8e91b3b813aa8c4
Maharshi-Yeluri <34574123+Maharshi-Yeluri@users.noreply.github.com>
2018-08-16 17:43:52 +0530

    intial commit

--- src/app/bs-navbar/bs-navbar.component.ts
+++ src/app/bs-navbar/bs-navbar.component.ts
@@ -1,3 +1,8 @@
+import { ShoppingCart } from './../models/shopping-cart';
+import { Observable } from 'rxjs/Observable';
+import { ShoppingCartService } from './../shopping-cart.service';
+import { AppUser } from './../models/app-user';
+import { AuthService } from './../auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +11,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bs-navbar.component.css']
 })
 export class BsNavbarComponent implements OnInit {
+  appUser: AppUser;
+  cart$: Observable<ShoppingCart>;
 
-  constructor() { }
+  constructor(private auth: AuthService, private shoppingCartService: ShoppingCartService) { 
+  }
 
-  ngOnInit() {
+  async ngOnInit() { 
+    this.auth.appUser$.subscribe(appUser => this.appUser = appUser);
+    this.cart$ = await this.shoppingCartService.getCart();
+  }
+
+  logout() {
+    this.auth.logout();
   }
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyAwxo8Bt7ZeYfFLI5WrakbNGtJsskBv3wA",
-    authDomain: "oshop-f3445.firebaseapp.com",
-    databaseURL: "https://oshop-f3445.firebaseio.com",
-    projectId: "oshop-f3445",
-    storageBucket: "oshop-f3445.appspot.com",
-    messagingSenderId: "223112932947"    
+    apiKey: "AIzaSyAikej2U_TC8rI4jFYLDYLPZWe-_r8eCkQ",
+    authDomain: "shopingcart-638c2.firebaseapp.com",
+    databaseURL: "https://shopingcart-638c2.firebaseio.com",
+    projectId: "shopingcart-638c2",
+    storageBucket: "shopingcart-638c2.appspot.com",
+    messagingSenderId: "140284951514" 
   }
 };
